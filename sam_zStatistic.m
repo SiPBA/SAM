be03cc99ae05b6957c0056721205c0c105c2a2fd
Fermín Segovia <fsegovia@ugr.es>
2020-07-20 13:15:09 +0200

    Update sam_zStatistic.m
    Minor fixes

--- sam_zStatistic.m
+++ sam_zStatistic.m
@@ -4,14 +4,13 @@
 %   [thres, p, zAcc, interval] = z_statistic(Acc, Pi0, alpha)
 % Input:
 %   acc:      Accuracy values (proportions)
-%   pi0:      Mean value for the known proportions (worst case corrected 
-%             and uncorrected) 
-%   alpha:    significance level
+%   bound:    Analytical upper bound 
+%   alpha:    Significance level
 % Outputs:
-%   thres:    threshold for significance
+%   thres:    Threshold for significance
 %   p:        p value
 %   zAcc:     Z-Statistic for a proportion test 
-%   interval: confidence interval 
+%   interval: Confidence interval 
 %
 % Please cite:
 %   J.M. Gorriz et al. for the Alzheimer's Disease Neuroimaging Initiative 
@@ -20,15 +19,20 @@
 %   CONCENTRATION INEQUALITIES. 
 %   doi: https://doi.org/10.1101/2019.12.27.889436.
 %
-function [thres, p, zAcc, interval] = sam_zStatistic(acc, pi0, alpha)
+function [thres, p, zAcc, interval] = sam_zStatistic(acc, bound, alpha)
+
+    accCor = acc' - bound;       % Accuracy values (corrected)
+    pi0 = mean([accCor; acc']);  % Mean value for the known proportions 
+                                 % (worst case corrected and uncorrected) 
 
     % Accuracies in columns
-    ind = acc > 0.5;  % Accuracies better than random, i.e. pi>0.5
+    ind = accCor > 0.5;  % Accuracies better than random, i.e. pi>0.5
+    %ind = accCor > mean(acc);  % Accuracies better than the mean.
     l = sum(ind);     % It considers only accuracies in confidence interval given by concentration inequalities
     %l = numel(Accn); % It considers all the accuracies, less conservative
 
-    se0 = sqrt(pi0*(1-pi0)/l); % standard error for a proportion
-    zAcc = (acc-pi0)/se0;      % z-statistic for a proportion
+    se0 = sqrt(pi0*(1-pi0)/l);   % standard error for a proportion
+    zAcc = (accCor-pi0)/se0;     % z-statistic for a proportion
 
     % Normality assumed on H0
     p = 1 - normcdf(zAcc,mean(zAcc(ind)),std(zAcc(ind)),'alpha',alpha); 
@@ -37,6 +41,7 @@ function [thres, p, zAcc, interval] = sam_zStatistic(acc, pi0, alpha)
     thres = norminv(p,mean(zAcc),std(zAcc));                           
 
     % Computing interval of this test
-    interval=[acc-1.96*sqrt(acc.*(1-acc)/l) acc+1.96*sqrt(acc.*(1-acc)/l)];
+    interval=[accCor-1.96*sqrt(accCor.*(1-accCor)/l) 
+              accCor+1.96*sqrt(accCor.*(1-accCor)/l)];
 
 end
